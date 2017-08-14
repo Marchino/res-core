3715e23696135aef740dd17469d7aaed0c7a24eb
Marco Crepaldi <marco.crepaldi@gmail.com>
2017-08-14 16:26:34 +0200

    adds import-from-email feature

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,8 @@
 class Review < ActiveRecord::Base
+  def self.import_from_email email
+    review_body = email.multipart? ? email.parts[0].body : email.body
+    create  from: email.from.join('; '),
+            subject: email.subject,
+            body: ActionController::Base.helpers.strip_tags(review_body.to_s)
+  end
 end
