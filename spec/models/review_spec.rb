1d01bc9136d01e48d7c6bbe64a11bbbc8df0daab
Marco Crepaldi <marco.crepaldi@gmail.com>
2017-08-21 16:23:41 +0200

    Review import (#2)
    * remove unused code
    
    * adds import-from-email feature
    
    * calculates score after importing email

---
+++ spec/models/review_spec.rb
@@ -0,0 +1,52 @@
+require 'spec_helper'
+
+describe "Review" do
+  let(:email_params){
+    { to: 'support@example.com',
+      from: 'me@example.com',
+      subject: 'product review',
+      body: 'this product is good' }
+  }
+  
+  it "can be imported from an email object" do
+    email = Mail.new email_params
+    expect{ Review.import_from_email email }.to change{ Review.count }.by 1
+    review = Review.last
+    expect(review.from).to eq(email_params[:from])
+    expect(review.subject).to eq(email_params[:subject])
+    expect(review.body).to eq(email_params[:body])
+  end
+
+  it "strips out html from mail body" do
+    email_params[:body] = '<p>This product is <b>good</b></p>'
+    email = Mail.new email_params
+    review = Review.import_from_email email
+    expect(review.body).to eq('This product is good')
+  end
+
+  it "imports from multipart email" do
+    email = Mail.new do
+      to      'support@example.com'
+      from    'Mikel Lindsaar <mikel@test.lindsaar.net.au>'
+      subject 'Review email'
+
+      text_part do
+        body 'This product is VERY GOOD'
+      end
+
+      html_part do
+        content_type 'text/html; charset=UTF-8'
+        body '<p>This product is <b>VERY GOOD</b></p>'
+      end
+    end
+    review = Review.import_from_email email
+    expect(review.body).to eq('This product is VERY GOOD')
+  end
+
+  it "assigns a score to the review when imported" do
+    email = Mail.new email_params
+    review = Review.import_from_email email
+    expect(review.score).to eq(100)
+  end
+
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,10 +2,19 @@ ENV["RAILS_ENV"] ||= 'test'
 
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+require 'mail'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
+Mail.defaults do
+  delivery_method :test
+end
+
+ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: ':memory:')
+ActiveRecord::Schema.verbose = false
+load "#{Rails.root.to_s}/db/schema.rb"
 
 RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
@@ -16,5 +25,4 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
-
 end
